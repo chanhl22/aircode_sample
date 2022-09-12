ab4edd17924e7d6eb1f11118c258bbef3bbe7ce6
chanhl22 <lch9502@naver.com>
2022-09-13 08:45:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/aircode/aircode_sample/AircodeSampleApplication.java
@@ -0,0 +1,13 @@
+package com.aircode.aircode_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircodeSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircodeSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aircode/aircode_sample/AircodeSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircode.aircode_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircodeSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
